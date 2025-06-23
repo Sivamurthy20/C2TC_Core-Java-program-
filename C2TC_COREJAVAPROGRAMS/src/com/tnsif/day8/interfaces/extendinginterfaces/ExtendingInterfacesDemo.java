5ef7a502c16e9006d3f58a334befe6841b743cd4
DELL <DELL@Nandhakumar.mshome.net>
2025-06-23 14:27:20 +0530

    this all code contain core java

---
+++ C2TC_COREJAVAPROGRAMS/src/com/tnsif/day8/interfaces/extendinginterfaces/ExtendingInterfacesDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.day8.interfaces.extendinginterfaces;
+
+public class ExtendingInterfacesDemo implements ChildInterface {
+	// Override ChildInterface method
+	public void print() { 
+		System.out.println("print method");
+	}
+	// Override InterfaceOne method
+	public void show() {
+		System.out.println("show Method");
+	}
+}
---
+++ C2TC_COREJAVAPROGRAMS/src/com/tnsif/day8/interfaces/extendinginterfaces/Sample.java
@@ -0,0 +1,9 @@
+package com.tnsif.day8.interfaces.extendinginterfaces;
+
+public class Sample {
+	public static void main(String[] args) {
+        ExtendingInterfacesDemo obj = new ExtendingInterfacesDemo();
+        obj.print(); 
+        obj.show();  
+    }
+}
